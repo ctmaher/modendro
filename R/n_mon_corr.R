62e814a3ec42c55e96360602da82f932d78b9aca
ctmaher <cmbristlecone@gmail.com>
2023-05-01 18:03:39 -0800

    Updated n_mon_corr() to work in any hemisphere

--- R/n_mon_corr.R
+++ R/n_mon_corr.R
@@ -34,10 +34,10 @@
 #'
 
 
-
-n_mon_corr <- function(chrono = NULL, clim = NULL, var = NULL,
-                       chrono.col = "xxxstd", agg.fun = "mean",
-                       max.lag = 3, corr.method = "pearson",
+n_mon_corr <- function(chrono = NULL, clim = NULL,
+                       var = NULL, clim.rel.per.begin = NULL,
+                       chrono.col = "std", agg.fun = "mean",
+                       max.lag = 2, corr.method = "pearson",
                        chrono.name = NULL, plots = TRUE){
 
   if (chrono.col %in% colnames(chrono)){
@@ -47,6 +47,8 @@ n_mon_corr <- function(chrono = NULL, clim = NULL, var = NULL,
          of the growth variable in the chronology data.frame")
   }
 
+  stopifnot("No valid climatically relevant period begin month provided (must be a numeric month)" =
+              is.numeric(clim.rel.per.begin))
 
   # make sure that "year" columns are labelled as such
   colnames(clim)[which((substr(colnames(clim), start = 1, stop = 2)
@@ -59,47 +61,69 @@ n_mon_corr <- function(chrono = NULL, clim = NULL, var = NULL,
 
   # make sure year and month are integers
   clim$year <- as.integer(clim$year)
-  chrono$year <- as.integer(chrono$year)
+  # I think some older versions of dplR included a year column, now it does not
+  # Get the year from row names
+  chrono$year <- rownames(chrono) |> as.integer()
+
+  # Make month an integer for now
   clim$month <- as.integer(clim$month)
 
-  # add water year for Oct-Dec
-  clim$water.year <- ifelse(clim$month %in% c(10,11,12), clim$year + 1, clim$year)
+  # Create a chronological sequence of months starting with the numeric month
+  # given as the clim.rel.per.begin argument
+  mon.seq <- rep(1:12, 2) # A list of months representing 2 whole calendar years.
+  mon.seq <- mon.seq[clim.rel.per.begin:length(mon.seq)][1:12] # 12 months in a row
+  # This seq of months will then be relevant to a particular current growth year, though the
+  # months span 2 calendar years. According to standard practice, for N hemisphere tree rings, the
+  # growth year is the later calendar year. In the S hemisphere, it is the earlier calendar year.
 
-  # Find min and max years for the correlations - for the plots later
-  min.y <- max(min(chrono$year), min(clim$year))
-  max.y <- min(max(chrono$year), max(clim$year))
+  # This should skip the water year concept in favor of the concept of a "growth year"
+  #clim$month <- factor(clim$month, levels = mon.seq)
+
+  clim$growyear <- if (clim.rel.per.begin %in% 1:6) {
+    offset <- clim.rel.per.begin - 1
+    c(rep(min(clim$year) - 1, offset), clim$year[1:(length(clim$year) - offset)])
+  } else {
+    offset <- 12 - clim.rel.per.begin + 1
+    c(clim$year[(offset + 1):length(clim$year)], rep(max(clim$year) + 1, offset))
+  }
+
+  # add water year for Oct-Dec
+  #clim$water.year <- ifelse(clim$month %in% c(10,11,12), clim$year + 1, clim$year)
+
+  # Find the complete years in the climate data
+  clim.complete <- aggregate(month ~ growyear, data = clim, length)
+  clim.complete <- clim.complete[clim.complete$month == 12,]
+  clim <- clim[clim$growyear %in% clim.complete$growyear,]
+
+  # Find min and max complete years for the correlations - for the plots later
+  min.y <- max(min(chrono$year), min(clim$growyear))
+  max.y <- min(max(chrono$year), max(clim$growyear))
 
   # Create a vector of all possible combinations of months
   # Regular calendar year first
-  mos.mat <- expand.grid(1:12,1:12)
-  mos <- apply(mos.mat, MARGIN = 1, FUN = function(x) seq(from = x[1], length.out = x[2]))
-  unreal <- lapply(mos, FUN = function(x) x < 13)
-  unreal2 <- lapply(unreal, FUN = function(x) length(x[x == F]))
-  mos <- mos[which(unreal2 < 1)]
+  mos.mat <- expand.grid(mon.seq, mon.seq)
+  mos <- apply(mos.mat, MARGIN = 1, FUN = \(x){
+    seq(from = which(mon.seq %in% x[1]),
+        to = which(mon.seq %in% x[2]))
+  })
+  # remove the non-ascending sequences
+  mos <- lapply(mos, FUN = \(x) {
+    if (x[1] > x[length(x)]) {
+      x <- NA
+    } else {
+      x
+    }
+  })
 
-  # Now water year months
-  wy <- c(10:12,1:9)
-  wy.list1 <- vector("list", 9) # 9 positions x 3 starting points
-  new.pos <- 4:12
-  for (i in seq_along(new.pos)){
-    wy.list1[[i]] <- wy[1:new.pos[i]]
-  }
-  wy.list2 <- vector("list", 9) # 9 positions x 3 starting points
-  for (i in seq_along(new.pos)){
-    wy.list2[[i]] <- wy[2:new.pos[i]]
-  }
-  wy.list3 <- vector("list", 9) # 9 positions x 3 starting points
-  for (i in seq_along(new.pos)){
-    wy.list3[[i]] <- wy[3:new.pos[i]]
-  }
-  # Lastly cover the single months and 10:12
-  wy.list0 <- list(10,11,12,c(10,11),c(11,12),c(10,11,12))
-  # put them together
-  wy.mos <- c(wy.list0,wy.list1,wy.list2,wy.list3)
+  mos <- mos[!is.na(mos)]
 
+  # These are indices, so now apply them to the mon.seq
+  mos <- lapply(mos, FUN = \(x) {
+    mon.seq[x]
+  })
 
-  # How to build in the annual lags?
-  # Perhaps I can hold the data.frame of results in a list, with the length of the list being equal to
+  # Annual lags?
+  # Hold the data.frame of results in a list, with the length of the list being equal to
   # the max lag?
   lag.seq <- 0:max.lag
   #lag.list <- vector("list", length(lag.seq))
@@ -109,79 +133,52 @@ n_mon_corr <- function(chrono = NULL, clim = NULL, var = NULL,
     # So I could make this a lot faster by not doing a loop.
 
     # calendar year
-    cal.cor.res <- ldply(mos, .fun = function(x){
+    cor.results <- ldply(mos, .fun = \(x){
       # Aggregate the variable of interest for the given month sequence
-      clim.mo <- aggregate(formula(paste(var, "year", sep = "~")), data = clim[clim$month %in% x,],
-                           FUN = function(z){ifelse(agg.fun %in% "mean", mean(z), sum(z))})
+      clim.mo <- aggregate(formula(paste(var, "growyear", sep = "~")),
+                           data = clim[clim$month %in% x, ],
+                           FUN = \(z){ifelse(agg.fun %in% "mean", mean(z), sum(z))})
       # attach the chronology to the climate data - enter the current lag before this step
       chrono.new <- chrono
       chrono.new$year <- chrono.new$year - l
-      clim.mo <- merge(clim.mo, chrono.new, by.x = "year", by.y = "year")
-      # Run the correlation test
-      # Need a reliable way to select the treering chronology variable
+      clim.mo <- merge(clim.mo, chrono.new, by.x = "growyear", by.y = "year")
+      # Run the correlation test between climate and the chronology
       ct <- cor.test(clim.mo[,var], clim.mo[, chrono.col], method = corr.method)
       # put the results together in a data.frame
       if (corr.method %in% "pearson") {
-        result <- data.frame(months = ifelse(length(x) > 1, deparse(x), paste(x)),
-                             coef = round(ct$estimate[[1]],3),
+
+        # which.months <- if (x[1] > x[length(x)]) {
+        #   paste(x[1], x[length(x)], sep = ":")
+        # }
+        result <- data.frame(months = ifelse(length(x) > 1,
+                                             paste(x[1], x[length(x)], sep = ":"),
+                                             paste(x)),
+                             coef = round(ct$estimate[[1]], 3),
                              p = ct$p.value[[1]],
                              ci.lo = ct$conf.int[1],
                              ci.hi = ct$conf.int[2])
       } else {
-        result <- data.frame(months = ifelse(length(x) > 1, deparse(x), paste(x)),
-                             coef = round(ct$estimate[[1]],3),
+        result <- data.frame(months = ifelse(length(x) > 1,
+                                             paste(x[1], x[length(x)], sep = ":"),
+                                             paste(x)),
+                             coef = round(ct$estimate[[1]], 3),
                              p = ct$p.value[[1]])
       }
       # return the result
       result
     })
-    cal.cor.res$sig <- ifelse(cal.cor.res$p > 0.05, "",
-                              ifelse(cal.cor.res$p <= 0.05 & cal.cor.res$p >= 0.01, "*",
-                                     ifelse(cal.cor.res$p <= 0.001, "***","**")))
-    cal.cor.res$seas.win <- "Calendar year"
 
+    cor.results$sig <- ifelse(cor.results$p > 0.05, "",
+                              ifelse(cor.results$p <= 0.05 & cor.results$p >= 0.01, "*",
+                                     ifelse(cor.results$p <= 0.001, "***", "**")))
 
-    # water year
-    wat.cor.res <- ldply(wy.mos, .fun = function(x){
-      # Aggregate the variable of interest for the given month sequence
-      clim.mo <- aggregate(formula(paste(var, "water.year", sep = "~")), data = clim[clim$month %in% x,],
-                           FUN = function(z){ifelse(agg.fun %in% "mean", mean(z), sum(z))})
-      colnames(clim.mo)[1] <- "year"
-      # attach the chronology to the climate data - enter the current lag before this step
-      chrono.new <- chrono
-      chrono.new$year <- chrono.new$year - l
-      clim.mo <- merge(clim.mo, chrono.new, by.x = "year", by.y = "year")
-      # Run the correlation test
-      ct <- cor.test(clim.mo[,var], clim.mo[, chrono.col], method = corr.method)
-      # put the results together in a data.frame
-      if (corr.method %in% "pearson") {
-        result <- data.frame(months = ifelse(length(x) > 1, paste(x[1], x[length(x)], sep = ":"), paste(x)),
-                             coef = round(ct$estimate[[1]],3),
-                             p = ct$p.value[[1]],
-                             ci.lo = ct$conf.int[1],
-                             ci.hi = ct$conf.int[2])
-      } else {
-        result <- data.frame(months = ifelse(length(x) > 1, paste(x[1], x[length(x)], sep = ":"), paste(x)),
-                             coef = round(ct$estimate[[1]],3),
-                             p = ct$p.value[[1]])
-      }
-      # return the result
-      result
-    })
-    wat.cor.res$sig <- ifelse(wat.cor.res$p > 0.05, "",
-                              ifelse(wat.cor.res$p <= 0.05 & wat.cor.res$p >= 0.01, "*",
-                                     ifelse(wat.cor.res$p <= 0.001, "***","**")))
-    wat.cor.res$seas.win <- "Water year"
-
-    cor.results <- rbind(cal.cor.res, wat.cor.res)
-    cor.results$lag <- ifelse(l == 0, paste(l),paste0("-",l))
+    cor.results$lag <- ifelse(l == 0, paste(l), paste0("-", l))
     cor.results
   })
 
   lag.list$lag <- factor(lag.list$lag, levels = lag.seq*-1)
-  lag.list <- lag.list[order(lag.list$coef, decreasing = T),] # sort by correlation coef
-  # There may be better ways to organize this that allow a breakdown of which monthly aggregates
-  # drive the strong correlations. Right now it is a lot to look at.
+  # sort by correlation coef
+  lag.list <- lag.list[order(lag.list$coef, decreasing = T),]
 
 
   if (plots == TRUE) {
@@ -196,29 +193,34 @@ n_mon_corr <- function(chrono = NULL, clim = NULL, var = NULL,
                                              strsplit(plot.df$months, ":")[[i]][1]))
     }
 
-    plot.df$start.mo <- ifelse(plot.df$seas.win %in% "Water year" & plot.df$start.mo %in% 12, 0, plot.df$start.mo)
-    plot.df$start.mo <- ifelse(plot.df$seas.win %in% "Water year" & plot.df$start.mo %in% 11, -1, plot.df$start.mo)
-    plot.df$start.mo <- ifelse(plot.df$seas.win %in% "Water year" & plot.df$start.mo %in% 10, -2, plot.df$start.mo)
-    plot.df$end.mo <- ifelse(plot.df$seas.win %in% "Water year" & plot.df$end.mo %in% 12, 0, plot.df$end.mo)
-    plot.df$end.mo <- ifelse(plot.df$seas.win %in% "Water year" & plot.df$end.mo %in% 11, -1, plot.df$end.mo)
-    plot.df$end.mo <- ifelse(plot.df$seas.win %in% "Water year" & plot.df$end.mo %in% 10, -2, plot.df$end.mo)
+    plot.df$start.mo <- unlist(plot.df$start.mo) |> as.numeric()
+    plot.df$end.mo <- unlist(plot.df$end.mo) |> as.numeric()
+
+    # plot.df$start.mo <- ifelse(plot.df$seas.win %in% "Water year" & plot.df$start.mo %in% 12, 0, plot.df$start.mo)
+    # plot.df$start.mo <- ifelse(plot.df$seas.win %in% "Water year" & plot.df$start.mo %in% 11, -1, plot.df$start.mo)
+    # plot.df$start.mo <- ifelse(plot.df$seas.win %in% "Water year" & plot.df$start.mo %in% 10, -2, plot.df$start.mo)
+    # plot.df$end.mo <- ifelse(plot.df$seas.win %in% "Water year" & plot.df$end.mo %in% 12, 0, plot.df$end.mo)
+    # plot.df$end.mo <- ifelse(plot.df$seas.win %in% "Water year" & plot.df$end.mo %in% 11, -1, plot.df$end.mo)
+    # plot.df$end.mo <- ifelse(plot.df$seas.win %in% "Water year" & plot.df$end.mo %in% 10, -2, plot.df$end.mo)
 
     plot.df$simp.sig <- ifelse(plot.df$sig %in% c("*","**","***"), "Sig.","Not sig.")
-    mo.xaxis <- c(10:12,1:12)
+
     title <- ifelse(is.null(chrono.name), paste0("Chronology correlations with ",var),
                     paste0(chrono.name, " chronology correlations with ",var))
 
-    out.plot <- ggplot(plot.df, aes(start.mo, coef, color = simp.sig)) +
+    out.plot <- ggplot(plot.df, aes(factor(start.mo, levels = mon.seq), coef, color = simp.sig)) +
       geom_point(shape = 5, size = 1.5) +
-      geom_point(data = plot.df, aes(end.mo, coef, color = simp.sig), shape = 124, size = 2) +
-      scale_x_continuous(breaks = c(-2:12), labels = mo.xaxis) +
+      geom_point(data = plot.df, aes(factor(end.mo, levels = mon.seq), coef, color = simp.sig), shape = 124, size = 2) +
+      scale_x_discrete(breaks = mon.seq, labels = mon.seq) +
       scale_y_continuous(breaks = seq(-1,1, by = 0.1)) +
       geom_segment(aes(xend = end.mo, yend = coef)) + xlab("Month") +
       ylab(paste0("Correlation coefficient\n(",corr.method,")")) +
       scale_color_manual(name = "", values = c("grey80","black")) +
       theme_bw() + facet_wrap(~lag, ncol = 1) +
       ggtitle(label = title,
-              subtitle = paste0("Monthly climate ", agg.fun, "s with annual lag 0:", max.lag," (", min.y,"-",max.y,")"))
+              subtitle = paste0("Monthly climate ",
+                                agg.fun, "s with annual lag 0:",
+                                max.lag," (", min.y,"-",max.y,")"))
 
     out.list <- list(lag.list, out.plot)
     return(out.list)
--- tests/testthat/test-n_mon_corr.R
+++ tests/testthat/test-n_mon_corr.R
@@ -1,3 +1,24 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Essential function works - and returns a vector", {
+  # Make some fake tree ring data
+  mat.test <- matrix(nrow = 50, ncol = 10)
+  rownames(mat.test) <- 1:50
+  colnames(mat.test) <- 1:10
+  mat.test <- apply(mat.test, MARGIN = 2, FUN = \(x) runif(length(x), 0.1, 2))
+  chrono <- dplR::chron(mat.test)
+  # Make some fake climate data
+  mo <- 1:12
+  x <- seq(-4, 4, length.out = 12)
+  gauss.curv <- \(x) {(10/sqrt(2*pi*1.6))*exp(-((x^2)/(2*1.6^2)))}
+  clim.mo <- data.frame(month = mo, clim.var = gauss.curv(x))
+  clim.list <- vector("list", length = 50)
+  for (y in seq_along(clim.list)) {
+    clim.y <- clim.mo
+    clim.y[,"clim.var"] <- clim.y[,"clim.var"] + runif(1, min = 0, max = 4)
+    clim.y$year <- y
+    clim.list[[y]] <- clim.y
+  }
+  clim <- do.call("rbind", clim.list)
+  expect_vector(n_mon_corr(chrono = chrono, clim = clim, var = "clim.var",
+             chrono.col = "std", chrono.name = "Synthetic"))
 })
+
