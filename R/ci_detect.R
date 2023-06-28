024d46511132cc9b31fdd3afd37853c1d61cf121
ctmaher <cmbristlecone@gmail.com>
2023-06-28 14:51:12 -0800

    Added a bare-bones version of the ci_detect() function and a supporting function, out_det_rem() that does the meat of the computation.

---
+++ R/ci_detect.R
@@ -0,0 +1,132 @@
+#' Curve intervention detection - statistical identification and removal of outliers in tree ring series
+#'
+#' @description
+#' This function implements an analysis based on the curve intervention detection techniques described by Druckenbrod et al,
+#' Rydval et al, and Rydval et al. The
+#'
+#'
+#' @param rwl A rwl object (read in by dplR's `read.rwl()`). Essentially a data.frame with columns names as series IDs and years as rownames.
+#' @param detrend.method A character vector specifying the detrending method to use. Passes to `dplR::detrend()`. Default is "AgeDepSpline".
+#' @param nyrs A numeric vector that determines the flexibility of the "AgeDepSpline" or the "Spline" detrending methods. Passes to `dplR::detrend()`. The default is 50 years.
+#' @param min.win The minimum outlier length in years (i.e., a moving window) to search for. The default is 9.
+#' @param max.win The maximum outlier length in years (i.e., a moving window) to search for. The default is 30.
+#' @param thresh The outlier detection threshold, corresponding to the number of devations from the mean. The default is 3.29, following Druckenbrod et al. 2013.
+#' @param out.span Parameter to determine the wiggliness of the loess splines fit to outlier periods. Higher numbers = more wiggles. Passes to `base::loess()`. The default is 1.
+#' @param max.iter The maximum number of iterations to run the outlier detection and removal processes. The default is 15.
+#'
+#' @details
+#'
+#'
+#' @return A 3-element list containing the "disturbance-free" series, the disturbance index, and a record of the outlier
+#' detection and removal iterations.
+#'
+#' @references
+#' Larsson & Larsson (2023) \emph{CDendro and CooRecorder programs of the CDendro package},
+#'  Cybis Elektronik & Data AB. https://www.cybis.se/forfun/dendro/index.htm
+#'
+#' @import dplR
+#' @import zoo
+#' @import stats
+#' @export
+#'
+#' @examples
+#' library(dplR)
+#' data("co021")
+#' # before
+#' ci_detect(co021, max.iter = 5)
+
+
+ci_detect <- function(rwl,
+                      detrend.method = "AgeDepSpline",
+                      nyrs = NULL,
+                      min.win = 9,
+                      max.win = 30,
+                      thresh = 3.29,
+                      out.span = 1,
+                      max.iter = 15) {
+
+  ## Run cp_detrend to power transform and detrend the rwl
+  cp_out <- cp_detrend(rwl, detrend.method = detrend.method, nyrs = nyrs)
+  # 1st element of cp_out is a rwl-data.frame of the residual transformed and detrended series-
+  # take this and turn it into a list, with NAs removed from each series
+  # Simplify = FALSE keeps the rownames (which are the years)
+  cp_list <- apply(cp_out[[1]], MARGIN = 2, simplify = FALSE, FUN = \(x) na.omit(x))
+
+  # Run the out_det_rem() process
+  # The for loop makes sense here - after the 1st iteration (based on the original data),
+  # each subsequent iteration uses the values generated in the previous iteration.
+  # Operations in the out_det_rem() function are vectorized, so this is reasonably efficient.
+  out_iter <- vector("list", length = max.iter + 1)
+  # The 1st element in the outlier iteration list is the initial data. This has to be a 2-element list
+  # to match the output of out_det_rem(). It is just an empty filler.
+  out_iter[[1]] <- list(cp_list, vector("list", length = length(cp_list)))
+  names(out_iter) <- 0:max.iter
+  for (i in 2:(max.iter+1)) {
+    out_iter[[i]] <- out_det_rem(out_iter[[i-1]][[1]], min.win = min.win, max.win = max.win, span = out.span)
+  }
+
+  ## Take the last element of out_iter as the final output series...
+  # add back the detrend curve
+  retrended <- mapply(FUN = \(x, y) {
+    x + na.omit(y)
+  }, x = out_iter[[length(out_iter)]][[1]], y = cp_out[["Detrend curves"]])
+
+  # & undo any transformation - this results in a "disturbance-free" series in original units
+  untransformed <- mapply(FUN = \(x, y) {
+    # Undo the transformations
+    if (x["action"] %in% "log10 transformed") {
+      10^y
+    } else {
+      if (x["action"] %in% "Power transformed") {
+        y^(1/as.numeric(x[["optimal.pwr"]]))
+      } else {
+        y
+      }
+    }
+  }, x = cp_out[["Metadata about transformations"]], y = retrended)
+
+  # Calculate the disturbance index -
+  # this is the difference between the disturbance-free series & the original series
+  dis_index <- mapply(FUN = \(x, y) {
+    na.omit(x) - y
+  }, x = rwl, y = untransformed, SIMPLIFY = FALSE)
+
+  # Restore the rwl-data.frame format for the disturbance-free and disturbance index series
+  # (with NAs for the years that aren't covered)
+  # 1st add a year column to each element of the lists
+  series_names <- names(untransformed)
+  untransformed_rwl0 <- mapply(FUN = \(x, y) {
+    x <- as.data.frame(x)
+    colnames(x) <- y
+    x[,"year"] <- rownames(x) |> as.numeric()
+    x
+  }, x = untransformed, y = series_names, SIMPLIFY = FALSE)
+
+  dis_index_rwl0 <- mapply(FUN = \(x, y) {
+    x <- as.data.frame(x)
+    colnames(x)[1] <- y
+    x[,"year"] <- rownames(x) |> as.numeric()
+    x
+  }, x = dis_index, y = series_names, SIMPLIFY = FALSE)
+
+  # Can apply merge() iteratively with Reduce():
+  untransformed_rwl <- Reduce(f = \(x, y) merge(x, y, by = "year", all = TRUE), untransformed_rwl0)
+
+  dis_index_rwl <- Reduce(f = \(x, y) merge(x, y, by = "year", all = TRUE), dis_index_rwl0)
+
+  # Make years as rownames
+  rownames(untransformed_rwl) <- untransformed_rwl$year
+  untransformed_rwl <- untransformed_rwl[,!c(colnames(untransformed_rwl) %in% "year")]
+
+  rownames(dis_index_rwl) <- dis_index_rwl$year
+  dis_index_rwl <- dis_index_rwl[,!c(colnames(dis_index_rwl) %in% "year")]
+
+  ## Last steps are to output the main results (disturbance-free, disturbance index, original series)
+  # and all of the iterations of the outlier removal process, with the trend curves, etc.
+  # all of these can then be plotted in the plot_ci_detect() function
+  ci_output_list <- list(untransformed_rwl, dis_index_rwl, out_iter)
+  names(ci_output_list) <- c("Disturbance-free series", "Disturbance index", "Outlier removal iterations")
+  ci_output_list
+
+} # End of the ci_detect() function
+
---
+++ R/out_det_rem.R
@@ -0,0 +1,254 @@
+#' Supporting function for `ci_detect()` - statistical identification and removal of outliers in tree ring series
+#'
+#' @description
+#' This function implements the outlier detection and premoval steps for the curve intervention detection techniques described by Druckenbrod et al,
+#' Rydval et al, and Rydval et al.
+#'
+#' @param rwi A rwl object of detrended standardized series produced by `cp_detrend()`.
+#' @param min.win The minimum outlier length in years (i.e., a moving window) to search for. The default is 9.
+#' @param max.win The maximum outlier length in years (i.e., a moving window) to search for. The default is 30.
+#' @param out.span Parameter to determine the wiggliness of the loess splines fit to outlier periods. Higher numbers = more wiggles. Passes to `stats::loess()`. The default is 1.
+#'
+#' @import dplR
+#' @import zoo
+#' @import stats
+#' @export
+
+
+
+
+## The outlier detection and removal process
+out_det_rem <- function(rwi, min.win = 9, max.win = 30, span = 1) {
+  ## Find the best AR model for each series
+  ar_fits <- lapply(rwi, FUN = \(x) ar(x, method = "burg", aic = TRUE, na.action = na.omit))
+
+  # Get the resids
+  ar_resids <- lapply(ar_fits, FUN = \(x) x$resid)
+
+  # Reverse the cp series
+  cp_rev <- lapply(rwi, rev)
+
+  ## "Backcast" the NAs (due to the ar order lag) at the beginning of each series
+  # Fit ar models of the same order as those above to the reversed data
+  comp_resids <- mapply(FUN = \(x, y) {
+    if (x$order > 0) { # If best fit order was 0, we don't need to do backcasting
+      br <- ar(y, order.max = x$order, method = "burg", aic = FALSE)$resid |>
+        rev() # flip it back to the correct chronological order
+      # subset just the ones that need to be filled in & attach to beginning of rest of resids
+      c(br[1:x$order], na.omit(x$resid)) |>
+        as.numeric()
+    } else { # If best fit order was 0, just return the AR resids (no backcasting).
+      x$resid
+    }
+  }, x = ar_fits, y = cp_rev, SIMPLIFY = FALSE)
+
+  # add the names back (these are the years) as rownames of data.frames
+  comp_resids <- mapply(FUN = \(x, y) {
+    rn <- names(x)
+    x <- as.data.frame(x)
+    colnames(x) <- "value"
+    rownames(x) <- rn
+    x[,"value"] <- y
+    x
+  }, x = rwi, y = comp_resids, SIMPLIFY = FALSE)
+
+  ## Compute moving window averages of various lengths - the length of the window corresponds to outlier length
+  # the min.win sets the smallest possible size, and tradition has it that 20 is the longest period. We can
+  # reassess that later - maybe the user can just specify this, within reasonable limits (max = 1/3 of series?).
+
+  mov_avgs <- lapply(comp_resids, \(x) {
+    # Option to use 1/3 the series length. This seems excessive for many tree ring series.
+    # if (is.null(max.win)) {
+    # max.win <- round(length(x$value)/3)
+    # }
+    win_lens <- min.win:max.win
+    ma_list <- lapply(win_lens, \(w) {
+      # Extract the values from the data frame
+      values <- x$value
+
+      # Compute the moving average using rollmean from the zoo package
+      ma <- rollmean(values, k = w, align = "left", fill = NA)
+
+      # Return the result
+      data.frame(value = ma)
+    })
+    # Pass on the window lengths as names
+    names(ma_list) <- win_lens
+    ma_list
+  })
+
+  # The outer level of mov_avgs are each moving window length, with the series inside each of those
+
+  ## Define upper and lower outlier thresholds for each moving window length
+  # The threshold is by default 3.29 robust scales from the robust mean of each moving average series
+  tbrms <- lapply(mov_avgs, FUN = \(x) {
+    lapply(x, FUN = \(x) {
+      TukeyBiweight(x$value, const = 9, na.rm = TRUE)
+      #tbrm(x$value, C = 9) # The dplR version
+    })
+  })
+
+  # The robust scale is from Hoaglin et al 1983, p417. Here I use the MAD as a simple substitute for now.
+  mads <- lapply(mov_avgs, FUN = \(x) {
+    lapply(x, FUN = \(x) {
+      mad(x$value, na.rm = TRUE)
+    })
+  })
+
+  lo_vals <- mapply(FUN = \(x, y) {
+    mapply(FUN = \(a, b) {
+      a - b*3.29
+    }, a = x, b = y, SIMPLIFY = FALSE)
+  }, x = tbrms, y = mads, SIMPLIFY = FALSE)
+
+  hi_vals <- mapply(FUN = \(x, y) {
+    mapply(FUN = \(a, b) {
+      a + b*3.29
+    }, a = x, b = y, SIMPLIFY = FALSE)
+  }, x = tbrms, y = mads, SIMPLIFY = FALSE)
+
+
+  ## Identify outliers for each series & each moving window size, select the largest among window sizes
+  # Outlier size is defined as the difference between the moving average and the threshold
+  # There is potential to bias detection toward the smallest window sizes if I use only the "raw" value of the
+  # moving average outliers. The better metric needs to be relative to the threshold for each particular window size.
+  # So that would be the abs of the difference between ma value & the (lo or hi) threshold.
+
+  # 1st step is to determine which ma values are outside of lo.vals & hi.vals, if any, for all ma window sizes
+  # These steps return index values for all outliers
+  pos_out <- mapply(FUN = \(x, y) {
+    mapply(FUN = \(a, b) {
+      # Find the index position of any outliers
+      index_pos <- which(a$value > b)
+
+      # Compute how big the outlier is
+      dev <- abs(a$value[index_pos] - b)
+
+      # Put the results in a data.frame
+      out_df <- data.frame(index_pos = index_pos, dev = dev)
+
+      # return just the largest outlier within each window size
+      out_df[which.max(out_df$dev),]
+
+    }, a = x, b = y, SIMPLIFY = FALSE)
+  }, x = mov_avgs, y = hi_vals, SIMPLIFY = FALSE)
+
+  neg_out <- mapply(FUN = \(x, y) {
+    mapply(FUN = \(a, b) {
+      # Find the index position of any outliers
+      index_pos <- which(a$value < b)
+
+      # Compute how big the outlier is
+      dev <- abs(a$value[index_pos] - b)
+
+      # Put the results in a data.frame
+      out_df <- data.frame(index_pos = index_pos, dev = dev)
+
+      # return just the largest outlier within each window size
+      out_df[which.max(out_df$dev),]
+
+    }, a = x, b = y, SIMPLIFY = FALSE)
+  }, x = mov_avgs, y = lo_vals, SIMPLIFY = FALSE)
+
+
+  # Now find the largest outliers for each series among all window sizes and record the window size and the index value.
+  # The index value will correspond to year.
+
+  # First the whole list for all window lengths
+  max_pos_outs <- lapply(pos_out, FUN = \(x) {
+    all_win_df <- do.call("rbind", x)
+    all_win_df$win_len <- as.numeric(rownames(all_win_df))
+    if (nrow(all_win_df) > 0) {
+      all_win_df$out_dir <- "pos"
+    }
+    all_win_df
+  })
+
+  max_neg_outs <- lapply(neg_out, FUN = \(x) {
+    all_win_df <- do.call("rbind", x)
+    all_win_df$win_len <- as.numeric(rownames(all_win_df))
+    if (nrow(all_win_df) > 0) {
+      all_win_df$out_dir <- "neg"
+    }
+    all_win_df
+  })
+
+  # Second the max among all window lengths (i.e., just 1 or no outlier for each series)
+  max_pos_out <- lapply(max_pos_outs, FUN = \(x) {
+    x[which.max(x$dev),]
+  })
+
+  max_neg_out <- lapply(max_neg_outs, FUN = \(x) {
+    x[which.max(x$dev),]
+  })
+
+  # Third find which is the largest of the pos and neg outliers
+  max_out <- mapply(FUN = \(x, y) {
+    if (nrow(x) > 0 & nrow(y) > 0) { # If there are both pos & neg outliers...
+      if (x$dev > y$dev) { # Chose largest of the two
+        x
+      } else {
+        y
+      }
+    } else { # if there aren't both
+      if (nrow(x) > 0) { # if there is only a pos outlier
+        x
+      } else {
+        if (nrow(y) > 0) { # if there is only a neg outlier
+          y
+        } else {
+          "No outliers detected"
+        }
+      }
+    }
+
+  }, x = max_pos_out, y = max_neg_out, SIMPLIFY = FALSE)
+
+  ## Fit curves to the largest outlier from each series (the resid detrended series in cp_list),
+  # subtract the outlier curve, store the records of everything.
+  out_curves <- mapply(FUN = \(x, y) {
+    # The index in max_out corresponds rows/elements in cp_list
+    # Control for the ones with no outliers
+    if (is.character(y)) {
+      out_period <- y
+    } else { # extract the outlier period and fit a curve
+      series_df <- data.frame(x)
+      colnames(series_df) <- "rwi"
+      series_df$year <- rownames(series_df) # keep this as a character for now
+      out_period <- series_df[y[,"index_pos"]:(y[,"index_pos"] + y$win_len - 1),]
+
+      # Start simple, with a loess spline
+      # Note: you can adjust the weight of each point using the weights argument
+      # This could help with mimicking the the Hugershoff-type curves (which are more flexible at the start).
+      # It might also make more sense to weight the last value more so that the residual is minimized.
+      # This would limit sharp jumps in the resulting series.
+      wts <- rep(1, nrow(out_period))
+      wts[nrow(out_period)] <- 4
+      curve <- loess(rwi ~ year, data = out_period, span = span, weights = wts)
+      out_period$curve <- curve$fitted
+      # "Correct" the rwi values for the outlier period by subtracting the fitted curve (aka, the residuals)
+      out_period$rwi.cor <- curve$residuals
+    }
+    # Return the results
+    out_period
+
+  }, x = rwi, y = max_out)
+
+  # Now the corrected rwi values can be inserted into the series to remove the outliers
+  rwi2 <- mapply(FUN = \(x, y) {
+    # Control for the ones with no outliers
+    if (is.character(y)) {
+      x # return the original series
+    } else {
+      x[y$year] <- y$rwi.cor # substitute the corrected section
+      x # Return the series
+    }
+  }, x = rwi, y = out_curves)
+
+  #rwi2
+
+  det_rem_out_list <- list(rwi2, out_curves)
+  names(det_rem_out_list) <- c("Corrected RWI", "Outlier metadata")
+  det_rem_out_list
+
+} # End of the out_det_rem() function
