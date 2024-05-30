e595aff040b869eb33a50495208ece963660a48e
ctmaher <cmbristlecone@gmail.com>
2024-05-30 09:23:41 -0800

    contained some directory-forming examples in plot_ci_detect

--- R/plot_ci_detect.R
+++ R/plot_ci_detect.R
@@ -65,28 +65,29 @@
 #' library(ggplot2)
 #'
 #' # For the iteration plots:
+#' \dontrun{
 #' dir.create("ca533_ci_iter_plots") # create a folder first
 #' # Then make plot sheets for each series
-#' # mapply(FUN = \(p, n) {
-#' # ggsave(
-#' # filename = paste0("ca533_ci_iter_plots/", n, "_ci_iter_plots.pdf"),
-#' # plot = marrangeGrob(p, nrow = length(p), ncol = 1),
-#' # width = 10, height = length(p)*4 # choose wise values here to make sure your plots
-#' # aren't squished
-#' # )
-#' # }, p = ca533_ci_plots[[1]][sapply(ca533_ci_plots[[1]], FUN = \(x) !is.character(x))],
-#' # n = names(ca533_ci_plots[[1]][sapply(ca533_ci_plots[[1]], FUN = \(x) !is.character(x))])
-#' # )
+#' mapply(FUN = \(p, n) {
+#' ggsave(
+#' filename = paste0("ca533_ci_iter_plots/", n, "_ci_iter_plots.pdf"),
+#' plot = marrangeGrob(p, nrow = length(p), ncol = 1),
+#' width = 10, height = length(p)*4 # choose wise values here to make sure your plots
+#' aren't squished
+#' )
+#' }, p = ca533_ci_plots[[1]][sapply(ca533_ci_plots[[1]], FUN = \(x) !is.character(x))],
+#' n = names(ca533_ci_plots[[1]][sapply(ca533_ci_plots[[1]], FUN = \(x) !is.character(x))])
+#' )
 #'
 #' # For the final plots (not run):
-#' # ggsave(
-#' # filename = "ca533_final_ci_plots.pdf",
-#' # plot = marrangeGrob(ca533_ci_plots[[2]][sapply(ca533_ci_plots[[2]],
-#' # FUN = \(x) !is.character(x))], nrow=1, ncol=1),
-#' # width = 10, height = 4 # choose wise values here to make sure your plots aren't squished
-#' # )
-#'
-#' # You can also use the `modendro` function `plot_cp_detrend()` to make plots of just intial
+#' ggsave(
+#' filename = "ca533_final_ci_plots.pdf",
+#' plot = marrangeGrob(ca533_ci_plots[[2]][sapply(ca533_ci_plots[[2]],
+#' FUN = \(x) !is.character(x))], nrow=1, ncol=1),
+#' width = 10, height = 4 # choose wise values here to make sure your plots aren't squished
+#' )
+#'}
+#' # You can also use the `modendro` function `plot_cp_detrend()` to make plots of just initial
 #' # detrending & transformaton steps.
 #' # The required input is the 4th element in the `ci_detect()` output.
 #' ca533_cp_plots <- plot_cp_detrend(ca533_ci[[5]])
